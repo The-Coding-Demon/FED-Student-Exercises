d5b4038b20291afd8434dccdfb0dae322205460e
The_Coding_Demon <ssmithmedia2015@gmail.com>
2023-04-17 10:35:08 +0100

    sums

---
+++ 3 - JS/Week 1/exercise1.js
@@ -0,0 +1,2 @@
+//this is code to run Hello World in java
+console.log("Hello, World!");
---
+++ 3 - JS/Week 1/exercise2.js
@@ -0,0 +1,6 @@
+//code to add two numbers together
+const sumpart1 = 436;
+const sumpart2 = 575;
+
+const sum = sumpart1 + sumpart2;
+console.log(sum);
