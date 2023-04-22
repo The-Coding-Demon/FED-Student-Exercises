d377e7a41673fca20fbefdb5cb336fca015e1a13
The_Coding_Demon <ssmithmedia2015@gmail.com>
2023-04-22 13:20:49 +0100

    sum change

--- 3 - JS/Week 1/exercise2.js
+++ 3 - JS/Week 1/exercise2.js
@@ -1,6 +1,10 @@
 //*code to add two numbers together
-const sumpart1 = 436;
-const sumpart2 = 575;
+const num1 = 436;
+const num2 = 575;
 
-const sum = sumpart1 + sumpart2;
-console.log(sum);
+const sum = (first, second) => {
+  let result = first + second;
+  console.log(result);
+};
+
+sum(num1, num2);
